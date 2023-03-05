ec20056a15728983db5c3c5f844c9f3c1a41c35b
Mateus Alves <mateus.alves.unb@gmail.com>
2023-03-05 15:26:49 +0100

    Typo in floaterm

--- after/plugin/floaterm.lua
+++ after/plugin/floaterm.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>ft", ":FloatermNew! --tittle=Terminalcr>")
+vim.keymap.set("n", "<leader>ft", ":FloatermNew! --tittle=Terminal<cr>")
 vim.keymap.set("n", "<leader>bft", ":FloatermNew! --tittle=workdir cd %:p:h<cr>")
 
 vim.keymap.set("n", "<leader>dft", ":FloatermNew! --tittle=dotfiles cd ${HOME}/versioning/dotfiles<cr>")
