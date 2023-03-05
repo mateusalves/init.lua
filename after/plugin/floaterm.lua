28bf166bd57a7be94097aace5910b08c65e740c1
Mateus Alves <mateus.alves.unb@gmail.com>
2023-03-05 17:48:09 +0100

    adding persistent terminal via floatterm

--- after/plugin/floaterm.lua
+++ after/plugin/floaterm.lua
@@ -1,4 +1,5 @@
-vim.keymap.set("n", "<leader>ft", ":FloatermNew! --tittle=Terminal<cr>")
+vim.keymap.set("n", "<leader>ft", ':FloatermToggle<CR>')
+vim.keymap.set("t", "<leader>ft", '<C-\\><C-n>:FloatermToggle<CR>')
 vim.keymap.set("n", "<leader>bft", ":FloatermNew! --tittle=workdir cd %:p:h<cr>")
 
 vim.keymap.set("n", "<leader>dft", ":FloatermNew! --tittle=dotfiles cd ${HOME}/versioning/dotfiles<cr>")
