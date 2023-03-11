82f51753f17a0ae08fdacc90cba998965bd003bb
Mateus Alves <mateus.alves.unb@gmail.com>
2023-03-11 13:59:38 +0100

    Adding other colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,12 +1,20 @@
-require('rose-pine').setup({
+--require('rose-pine').setup({
+    --disable_background = true
+--})
+require("catppuccin").setup {
+    flavour = "macchiato", -- mocha, macchiato, frappe, latte
     disable_background = true
-})
+}
+
+vim.api.nvim_command "colorscheme catppuccin"
 
 function ColorMyPencils(Color)
-	color = color or "rose-pine"
+	--color = color or "rose-pine"
+	color = color or "catppuccin"
 	vim.cmd.colorscheme(color)
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
+
--- lua/kirch/plugins.lua
+++ lua/kirch/plugins.lua
@@ -22,6 +22,8 @@ return require('packer').startup(function(use)
             vim.cmd('colorscheme rose-pine')
         end
     })
+    use 'catppuccin/nvim'
+
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use("nvim-treesitter/nvim-treesitter-context");
     use('ThePrimeagen/harpoon')
