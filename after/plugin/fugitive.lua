a3db63e0cd8b82df8e745f4ed03c660a83956dce
Mateus Alves <mateus.alves.unb@gmail.com>
2025-03-03 14:50:35 -0300

    Git status open vertical

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,4 +1,5 @@
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = '[g]it [s]tatus' })
+--vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = '[g]it [s]tatus' })
+vim.keymap.set("n", "<leader>gs", ":vertical Git<cr>", { desc = '[g]it [s]tatus' })
 vim.keymap.set("n", "<leader>gds", ":vertical Gdiffsplit<cr>", { desc = '[g]it [d]iffer [s]tatus' })
 vim.keymap.set("n", "gh", "<cmd>diffget //2<cr>", {desc = 'get diff from left side'})
 vim.keymap.set("n", "gj", "<cmd>diffget //3<cr>", {desc = 'get diff from right side'})
